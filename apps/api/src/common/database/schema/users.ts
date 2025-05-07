9018f352c6dac2190a776084e69dc3f016ca5d6f
Adolfo <adolfoklk0307@gmail.com>
2025-05-06 20:04:17 -0400

    Pequeños cambios del userSchema

--- apps/api/src/common/database/schema/users.ts
+++ apps/api/src/common/database/schema/users.ts
@@ -3,7 +3,9 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { shoppingLists } from './shopping-lists'
 
 export const users = sqliteTable('users', {
-  id: text().primaryKey().notNull(),
+  id: text()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
   username: text().notNull().unique()
 })
 
