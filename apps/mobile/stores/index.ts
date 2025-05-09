dece851e2efbf5d174fafc8d7eee2b29e2d6f8f8
Adolfo <adolfoklk0307@gmail.com>
2025-05-09 02:26:17 -0400

    cambiando manejo de estado global

--- apps/mobile/stores/index.ts
+++ apps/mobile/stores/index.ts
@@ -1,15 +1,36 @@
-import { setItemAsync } from 'expo-secure-store'
+import { ShoppingList, User } from '@/types'
 import { create } from 'zustand'
 
-interface TokenStore {
-  token: string | null
-  setToken: (token: string | null) => void
+interface UserStore {
+  user: User | null
+  setUser: (user: User) => void
 }
 
-export const useStore = create<TokenStore>()((set) => ({
-  token: null,
-  setToken: async (token) => {
-    set({ token })
-    await setItemAsync('token', token || '')
-  }
+interface ShoppingListsStore {
+  shoppingLists: ShoppingList[]
+  setShoppingLists: (shoppingLists: ShoppingList[]) => void
+  addShoppingList: (list: ShoppingList) => void
+  removeShoppingList: (id: number) => void
+}
+
+export const useStore = create<UserStore & ShoppingListsStore>()((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  shoppingLists: [],
+  setShoppingLists: (shoppingLists) =>
+    set((state) =>
+      JSON.stringify(state.shoppingLists) !== JSON.stringify(shoppingLists)
+        ? { shoppingLists }
+        : state
+    ),
+  addShoppingList: (list) =>
+    set((state) => ({
+      ...state,
+      shoppingLists: [...state.shoppingLists, list]
+    })),
+  removeShoppingList: (id) =>
+    set((state) => ({
+      ...state,
+      shoppingLists: state.shoppingLists.filter((list) => list.id !== id)
+    }))
 }))
