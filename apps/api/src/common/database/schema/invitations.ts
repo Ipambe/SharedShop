f2db64bf8b05ae81e888493b5d67f0a0c3a40e72
Adolfo <adolfoklk0307@gmail.com>
2025-05-26 18:04:40 -0400

    Removiendo idea de una tabla para las invitaciones

--- apps/api/src/common/database/schema/invitations.ts
+++
@@ -1,7 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
-
-export const invitations = sqliteTable('invitations', {
-  id: integer().primaryKey({ autoIncrement: true }).notNull(),
-  token: text().notNull(),
-  isSingleUse: integer({ mode: 'boolean' }).notNull()
-})
