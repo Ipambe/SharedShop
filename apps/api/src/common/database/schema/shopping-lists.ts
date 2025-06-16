4c4f17512229c53bd68ef43abacecf8412596af3
Adolfo <adolfoklk0307@gmail.com>
2025-06-16 17:59:54 -0400

    Add invitation relation to shopping lists schema

--- apps/api/src/common/database/schema/shopping-lists.ts
+++ apps/api/src/common/database/schema/shopping-lists.ts
@@ -2,13 +2,18 @@ import { shoppingListMembers } from './shopping-list-members'
 import { relations } from 'drizzle-orm'
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
 import { products } from './products'
+import { invitations } from './invitation'
 
 export const shoppingLists = sqliteTable('shopping-lists', {
   id: integer().primaryKey({ autoIncrement: true }).notNull(),
   name: text().notNull()
 })
 
-export const shoppingListsRelations = relations(shoppingLists, ({ many }) => ({
-  members: many(shoppingListMembers),
-  products: many(products)
-}))
+export const shoppingListsRelations = relations(
+  shoppingLists,
+  ({ one, many }) => ({
+    members: many(shoppingListMembers),
+    products: many(products),
+    invitation: one(invitations)
+  })
+)
